b14019d0d180b73b72c6936b23d002356aa7e5be
mahdisadjadi <sadjadi.seyedmahdi@gmail.com>
2021-12-28 20:14:06 -0800

    Improve style

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,17 +1,16 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import arxivscraper
 
+
 def test_main():
-    scraper = (
-    arxivscraper
-    .Scraper(
-        category='physics:cond-mat', 
-        date_from='2017-05-27',
-        date_until='2017-05-30'
-        )
+    """Test main function. The output should 97 entries.
+    """
+    scraper = arxivscraper.Scraper(
+        category="physics:cond-mat", date_from="2017-05-27", date_until="2017-05-30"
     )
 
     output = scraper.scrape()
