0ce7c6802312ec03ce108d5fea5d532574e1100f
mahdisadjadi <sadjadi.seyedmahdi@gmail.com>
2021-12-28 20:09:46 -0800

    remove arg from test_main

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -4,7 +4,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 import arxivscraper
 
-def test_main(x=1):
+def test_main():
     scraper = (
     arxivscraper
     .Scraper(
