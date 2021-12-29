48dfca4a472dc771ebe372170a09d078c72c7032
mahdisadjadi <sadjadi.seyedmahdi@gmail.com>
2021-12-28 19:50:49 -0800

    fix numpy not imported bug

--- arxivscraper/arxivscraper.py
+++ arxivscraper/arxivscraper.py
@@ -216,6 +216,7 @@ def search_all(df, col, *words):
     Return a sub-DataFrame of those rows whose Name column match all the words.
     source: https://stackoverflow.com/a/22624079/3349443
     """
+    import numpy as np
     return df[np.logical_and.reduce([df[col].str.contains(word) for word in words])]
 
 
